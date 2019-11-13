9e2f98b32536c9e1ebf53927b9e84687259dad31
rnikolai <rubtsov_nikolay@mail.ru>
2019-11-13 11:07:33 -0500

    testing disable

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var w = 1;
 var Size = Math.floor((innerWidth - 100) / w);
 
 var gap = false;
-var lockdown = false; //needed to terminate the running sorts
+var running = false; //there are corrently working sorters
 
 class Bar {
   constructor(x, y, val, state) {
@@ -19,14 +19,15 @@ class Bar {
     this.val = val;
     this.state = 1;
   }
-
   draw() {
     if (this.state == 0) {
-      c.fillStyle = "#e61919";//red
+      c.fillStyle = "#e61919";
     } else if (this.state == 1) {
-      c.fillStyle = "#4CAF50";//green
+      c.fillStyle = "#4CAF50";
     } else if ((this.state = -1)) {
       c.fillStyle = "white";
+    } else {
+      c.fillStyle = "ED6FFB7";
     }
 
     c.fillRect(this.x, this.y, w, this.val);
@@ -34,11 +35,13 @@ class Bar {
 }
 
 var Bars = [];
-var Bars_c = [];
-var sort_i = [];
 
 //lockdown
 
+function disableElement() {
+  document.getElementById("new").disabled = true;
+}
+s
 range.onclick = function () {
 
   w = 101 - range.value; //101 is based on sliders max value
@@ -53,29 +56,26 @@ document.getElementById("new").onclick = function () {
 
 document.getElementById("qs").onclick = function () {
   quickSort(Bars, 0, Bars.length - 1);
-  console.log(sort_i);
 };
 
 document.getElementById("ss").onclick = function () {
   slowSort(Bars, 0, Bars.length - 1);
 };
-
+/*
 document.getElementById("bs").onclick = function () {
   bubbleSort(Bars, 0, Bars.length - 1);
 };
+*/
 
-document.getElementById("finish").onclick = function () {
-  finish();
-}
 //qs
 
-// This sort will call itself recursevly
-
 async function quickSort(arr, start, end) {
-  if (start >= end || lockdown == true) {
+  if (start >= end) {
+    for (i = 0; i <= Bars.lengthi; i++) Bars[i].state = -1;
+    Redraw();
     return;
   }
-  var index = await partition(arr, start, end);// have to wait for an index before running anything else
+  var index = await partition(arr, start, end);
   Bars[index].state = -1;
 
   await Promise.all([
@@ -94,14 +94,14 @@ async function partition(arr, start, end) {
   Bars[pivotIndex].states = 0;
   for (var i = start; i < end; i++) {
     if (arr[i].val < pivotValue) {
-      await addInst(arr, i, pivotIndex);
+      await swap(arr, i, pivotIndex);
 
       Bars[pivotIndex].state = -1;
       pivotIndex++;
       Bars[pivotIndex].state = 0;
     }
   }
-  await addInst(arr, pivotIndex, end);
+  await swap(arr, pivotIndex, end);
 
   for (var i = start; i < end; i++) {
     if (i != pivotIndex) {
@@ -114,38 +114,35 @@ async function partition(arr, start, end) {
 //ss
 
 async function slowSort(arr, start, end) {
-  if (start >= end || lockdown == true) {
+  if (start >= end) {
+    for (i = 0; i <= Bars.lengthi; i++) Bars[i].state = -1;
+    Redraw();
     return;
   }
 
   var index = await partitionS(arr, start, end);
-  if (index = -1) {
-    return;
-  }
   Bars[index].state = -1;
 
   await slowSort(arr, start, index - 1), await slowSort(arr, index + 1, end);
 }
-
 async function partitionS(arr, start, end) {
-  if (lockdown == true) {
-    return -1;
+  for (var i = start; i < end; i++) {
+    Bars[i].state = 1;
   }
 
   var pivotValue = arr[end].val;
   var pivotIndex = start;
   Bars[pivotIndex].states = 0;
-
   for (var i = start; i < end; i++) {
     if (arr[i].val < pivotValue) {
-      await addInst(arr, i, pivotIndex);
+      await swap(arr, i, pivotIndex);
 
       Bars[pivotIndex].state = -1;
       pivotIndex++;
       Bars[pivotIndex].state = 0;
     }
   }
-  await addInst(arr, pivotIndex, end);
+  await swap(arr, pivotIndex, end);
 
   for (var i = start; i < end; i++) {
     if (i != pivotIndex) {
@@ -165,7 +162,7 @@ async function bubbleSort() {
       Bars[j].state = -1;
 
       if (Bars[j].val > Bars[j + 1].val) {
-        addInst(Bars, j, j + 1);
+        swap(Bars ,j, j+1);
       }
       Bars[j].state = 0;
     }
@@ -177,17 +174,14 @@ async function bubbleSort() {
 
 function generateNewArray() {
   Bars.splice(0, Bars.length);
-  Bars_c.splice(0, Bars.length);
-  c.clearRect(0, 0, innerWidth, innerHeight); // remove old canvas img
+  c.clearRect(0, 0, innerWidth, innerHeight); // remove old
   for (i = 0; i < Size; i++) {
     Bars.push(
       new Bar(50 + i * (w + gap), 50, Math.round(Math.random() * 500) + 5, 0)
     );
   }
-  Bars_c = Bars;
   Redraw();
-  console.log(Bars_c);
-  console.log(Bars);
+  //console.log(Bars);
 }
 
 generateNewArray();
@@ -199,24 +193,13 @@ function Redraw() {
   }
 }
 
-async function addInst(arr, a, b) {
-  sort_i.push({ a, b });// store each operation for animation
-  var temp = arr[a].val;
-  arr[a].val = arr[b].val;
-  arr[b].val = temp;
-}
-
 async function swap(arr, a, b) {
   var temp = arr[a].val;
   arr[a].val = arr[b].val;
   arr[b].val = temp;
+  Redraw();
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-//since speed can be modified during runtime , we can speed everything up when we need to finish the proces
-function finish() {
-  lockdown = true;
 }

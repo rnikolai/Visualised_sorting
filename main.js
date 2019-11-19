e9d7e977cf71cad872f546b7be5ee7ca0f27c565
rnikolai <rubtsov_nikolay@mail.ru>
2019-11-19 17:08:48 -0500

    resume ready

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ var c = canvas.getContext("2d");
 var range = document.getElementById("range");
 var speed = document.getElementById("speed");
 
-var widthOfBar = 1;
-var Size = Math.floor((innerWidth - 100) / widthOfBar);
+var gapBox = document.getElementById("gap");
+var gap = 0;
 
-var gap = false;
+var time = -1;
+
+var widthOfBar = 1;
+var Size = Math.floor((innerWidth - 100) / (widthOfBar + gap));
+
 var running = false; //there are corrently working sorters
 
 class Bar {
@@ -17,15 +21,15 @@ class Bar {
     this.x = x;
     this.y = y;
     this.val = val;
-    this.state = 0;
+    this.state = -1;
   }
   draw() {
     switch (this.state) {
       case 0:
-        c.fillStyle = "#4192D9";
+        c.fillStyle = "#4CAF50";
         break;
       case 1:
-        c.fillStyle = "white";
+        c.fillStyle = "blue";
         break;
       default:
         c.fillStyle = "white";
@@ -46,6 +50,7 @@ function disableElement() {
   document.getElementById("qs").setAttribute("disabled", "disabled");
   document.getElementById("ss").setAttribute("disabled", "disabled");
   document.getElementById("range").setAttribute("disabled", "disabled");
+  document.getElementById("gap").setAttribute("disabled", "disabled");
 }
 
 function enableElement() {
@@ -53,16 +58,26 @@ function enableElement() {
   document.getElementById("qs").removeAttribute("disabled");
   document.getElementById("ss").removeAttribute("disabled");
   document.getElementById("range").removeAttribute("disabled");
+  document.getElementById("gap").removeAttribute("disabled");
 }
 
-range.onclick = function () {
-  widthOfBar = Math.floor((innerWidth - 100) / range.value); //101 is based on sliders max value
-  Size = Math.floor((innerWidth - 100) / widthOfBar); //TO-DO add a size control
+gapBox.onclick = function () {
+  gap = (gapBox.checked) ? 1 : 0;
   generateNewArray();
 };
 
-document.getElementById("new").onclick = function () {
+function newSize() {
+  widthOfBar = Math.floor((innerWidth - 100) / range.value + gap);
+  Size = Math.floor((innerWidth - 100) / (widthOfBar));
   generateNewArray();
+}
+//range and new are same need new method
+range.onclick = function () {
+  newSize();
+};
+
+document.getElementById("new").onclick = function () {
+  newSize();
 };
 
 document.getElementById("qs").onclick = async function () {
@@ -70,22 +85,32 @@ document.getElementById("qs").onclick = async function () {
   var t0 = performance.now();
   await quickSort(Bars, 0, Bars.length - 1);
   var t1 = performance.now();
-  console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
+  time = t1 - t0;
   enableElement();
-  console.log(Bars_c);
-  
-  
-  //Redraw();
+  console.log(time + " " + Bars_c.length + " " + Bars.length);
 };
 
-document.getElementById("ss").onclick = function () {
-  replace(Bars, 1, 10);
+document.getElementById("ss").onclick = async function () {
+  disableElement();
+  var t0 = performance.now();
+  await slowSort(Bars, 0, Bars.length - 1);
+  var t1 = performance.now();
+  time = t1 - t0;
+  enableElement();
+  console.log(time + " " + Bars_c.length + " " + Bars.length);
 };
-/*
-document.getElementById("bs").onclick = function () {
-  bubbleSort(Bars, 0, Bars.length - 1);
+
+document.getElementById("bs").onclick = async function () {
+  disableElement();
+  var t0 = performance.now();
+  await bubbleSort(Bars, 0, Bars.length);
+  var t1 = performance.now();
+  time = t1 - t0;
+  enableElement();
+  console.log(time + " " + Bars_c.length + " " + Bars.length);
+
 };
-*/
+
 
 //qs
 
@@ -94,12 +119,24 @@ async function quickSort(arr, start, end) {
     return;
   }
   var index = await partition(arr, start, end);
+
   await Promise.all([
     quickSort(arr, start, index - 1),
-    sleep(speed.value),
     quickSort(arr, index + 1, end)
   ]);
-  
+}
+
+//ss
+
+async function slowSort(arr, start, end) {
+  if (start >= end) {
+    return;
+  }
+
+  var index = await partition(arr, start, end);
+
+  await slowSort(arr, start, index - 1);
+  await slowSort(arr, index + 1, end);
 }
 
 async function partition(arr, start, end) {
@@ -115,54 +152,25 @@ async function partition(arr, start, end) {
   return pivotIndex;
 }
 
-//ss
-
-async function slowSort(arr, start, end) {
-  if (start >= end) {
-    return;
-  }
-
-  var index = await partitionS(arr, start, end);
-
-  await slowSort(arr, start, index - 1);
-  await slowSort(arr, index + 1, end);
-}
-async function partitionS(arr, start, end) {
-  var pivotValue = arr[end].val;
-  var pivotIndex = start;
-  for (var i = start; i < end; i++) {
-    if (arr[i].val < pivotValue) {
-      await swap(arr, i, pivotIndex);
-      pivotIndex++;
-    }
-  }
-  await swap(arr, pivotIndex, end);
-  return pivotIndex;
-}
-
 //bs
 
-/* async function bubbleSort() {
-  for (i = 0; i < Bars.length; i++) {
-    arr[i].state = 0;
-    j = 0;
-    for (j = 0; j < Bars.length - i - 1; j++) {
-      arr[j].state = -1;
-
+async function bubbleSort(arr, start, end) {
+  for (i = 0; i < end; i++) {
+    for (j = 0; j < end - i - 1; j++) {
       if (arr[j].val > arr[j + 1].val) {
-        swap(Bars, j, j + 1);
+        await swap(arr, j, j + 1);
       }
-      Bars[j].state = 0;
     }
-    Bars[i].state = 1;
+    j = 0;
   }
 }
-*/
+
 
 //shared functions
 
 function generateNewArray() {
   Bars.splice(0, Bars.length);
+  Bars_c.splice(0, Bars_c.length);
   c.clearRect(0, 0, innerWidth, innerHeight); // remove old
   for (i = 0; i < Size; i++) {
     Bars.push(
@@ -183,21 +191,25 @@ function Redraw() {
 }
 
 async function swap(arr, a, b) {
-  Bars_c.push({a,b});
+  var t3 = performance.now();
+  await delay(speed.value);
+  var t4 = performance.now();
+  Bars_c.push({ a, b });
   var temp = arr[a].val;
   arr[a].val = arr[b].val;
   arr[b].val = temp;
+  swapOnGraph(arr, a, b);
   
-  replace(arr, a, b);
+  console.log(t4 - t3);
 }
 
-async function replace(arr, a, b) {
+function swapOnGraph(arr, a, b) {
   c.clearRect(arr[a].x, arr[a].y, widthOfBar, 505);
-  Bars[a].draw();
   c.clearRect(arr[b].x, arr[b].y, widthOfBar, 505);
-  Bars[b].draw();
+  arr[a].draw();
+  arr[b].draw();
 }
 
-function sleep(ms) {
+function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
